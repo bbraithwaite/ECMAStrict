2283c2b187bebfcad8f065e03f6714ca059c1adf
bbraithwaite <bradley.braithwaite@gmail.com>
2015-03-18 23:35:08 +0100

    added puntuators to lexer

--- lexer.py
+++ lexer.py
@@ -1,8 +1,8 @@
 import ply.lex as lex 
 
-tokens = (
-    'IDENTIFIER',       # identifier
-    # Keywords
+#   The following tokens are ECMAScript keywords and may not be 
+#   used as Identifiers in ECMAScript programs.
+keywords = (
     'BREAK',            # break
     'CASE',             # case
     'CATCH',            # catch
@@ -28,16 +28,26 @@ tokens = (
     'THIS',             # this
     'WITH',             # with
     'THROW',            # throw
-    'TRY',              # try
-    # Future Keywords
+    'TRY'               # try
+)
+
+#   The following words are used as keywords in proposed extensions 
+#   and are therefore reserved to allow for the possibility of 
+#   future adoption of those extensions. 
+future_keywords = (
     'CLASS',            # class
     'ENUM',             # enum
     'EXTENDS',          # extends
     'SUPER',            # super
     'CONST',            # const
     'EXPORT',           # export
-    'IMPORT',           # import
-    # Future Strict Keywords
+    'IMPORT'            # import
+)
+
+#   The following tokens are also considered to be FutureReservedWords 
+#   when they occur within strict mode code.
+#   NB this parser defaults to strict mode.
+future_strict_keywords = (
     'IMPLEMENTS',       # implements
     'LET',              # let
     'PRIVATE',          # private
@@ -49,69 +59,106 @@ tokens = (
     'STATIC'            # static
 )
 
+
+punctuators = (
+    'LBRACE',           # {
+    'RBRACE',           # }
+    'LPAREN',           # (
+    'RPAREN',           # )
+    'LBRACKET',         # [
+    'RBRACKET',         # ]
+    'DOT',              # .
+    'SEMICOLON',        # ;
+    'COMMA'             # ,
+)
+
+punctuators_arithmetic = (
+    'PLUS',             # +
+    'MINUS',            # -
+    'MULTIPLY',         # *
+    'MODULO',           # %
+    'PLUSPLUS',         # ++
+    'MINUSMINUS'        # --
+)
+    
+punctuators_assignment = (
+    'EQUAL',            # =
+    'PLUSEQUAL',        # +=
+    'MINUSEQUAL',       # -=
+    'MULTIPLYEQUAL',    # *=
+    'MODULOEQUAL',      # %=
+    'LSHIFTEQUAL',      # <<=
+    'RSHIFTEQUAL',      # >>=
+    'LOGRSHIFTEQUAL',   # >>>=
+    'ANDEQUAL',          # &=
+    'OREQUAL',          # |=
+    'XOREQUAL'          # ^=
+)
+
+punctuators_bitwise = (
+    'LSHIFT',           # <<
+    'RSHIFT',           # >>
+    'LOGRSHIFT',        # >>>
+    'AND',              # &
+    'OR',               # |
+    'XOR',              # ^
+    'BWNOT',            # ~
+)
+
+punctuators_comparison = (
+    'LT',               # <
+    'GT',               # >
+    'LTE',              # <=
+    'GTE',              # >=,
+    'EQUALV',           # ==  (value, with type coercion)
+    'NOTEQUALV',        # !=  (value, with type coercion)
+    'EQUALVT',          # === (value and type)
+    'NOTEQUALVT'        # !== (value and type)
+)
+
+punctuators_conditional = (
+    'TERNARY',          # ?
+    'COLON'             # :
+)
+
+punctuators_logical = (
+    'ANDAND',           # &&,
+    'NOT',              # !
+    'OROR',             # ||
+)
+
+div_punctuators = (
+    'FSLASH',           # /
+    'FSLASHEQUAL'       # /=
+)
+
+tokens = (
+    'IDENTIFIER',
+)
+
+punctuators = (
+    punctuators + 
+    punctuators_assignment + 
+    punctuators_arithmetic + 
+    punctuators_bitwise + 
+    punctuators_conditional + 
+    punctuators_comparison + 
+    punctuators_logical + 
+    div_punctuators
+)
+
+tokens = (
+    tokens + 
+    keywords + 
+    future_keywords + 
+    future_strict_keywords + 
+    punctuators
+)
+
 def t_single_line_comment(t):
     # Only single line comments are supported
     r'^//.*'
     pass
-
-#   The following tokens are ECMAScript keywords and may not be 
-#   used as Identifiers in ECMAScript programs.
-keywords = [
-    'break', 
-    'case', 
-    'catch', 
-    'continue', 
-    'debugger', 
-    'default', 
-    'delete', 
-    'do',
-    'else', 
-    'finally',
-    'for',
-    'function',
-    'if',
-    'in',
-    'instanceof',
-    'typeof',
-    'new',
-    'return',
-    'var', 
-    'void',
-    'switch',
-    'while',
-    'this',
-    'with',
-    'throw',
-    'try'
-]
-
-#   The following words are used as keywords in proposed extensions 
-#   and are therefore reserved to allow for the possibility of 
-#   future adoption of those extensions.  
-future_keywords = [
-    'class', 
-    'enum', 
-    'extends', 
-    'super',
-    'const',
-    'export',
-    'import'
-]
-
-#   The following tokens are also considered to be FutureReservedWords 
-#   when they occur within strict mode code.
-#   NB this parser defaults to strict mode.
-future_strict_keywords = [
-    'implements', 
-    'let', 
-    'private',
-    'public',
-    'yield',
-    'interface',
-    'package',
-    'protected',
-    'static'    
-]
 
 def t_IDENTIFIER(t):
     # identifiers must start with:
@@ -124,18 +171,81 @@ def t_IDENTIFIER(t):
     #   Unicode digit (0-9)
     #   (Other unicode chars are possible but not supported)
     r'[a-zA-Z$_][a-zA-Z$_0-9]*'
-    if t.value in keywords:
+    v = t.value.upper() 
+    if v in keywords:
         t.type = t.value.upper()
 
-    if t.value in future_keywords:
+    if v in future_keywords:
         t.type = t.value.upper()
         
-    if t.value in future_strict_keywords:
+    if v in future_strict_keywords:
         t.type = t.value.upper()
 
     return t
 
-t_ignore                = ' \t'
+# punctuators
+t_LBRACE        = r'{'
+t_RBRACE        = r'}'
+t_LPAREN        = r'\('
+t_RPAREN        = r'\)'
+t_LBRACKET      = r'\['
+t_RBRACKET      = r'\]'
+t_DOT           = r'\.'
+t_SEMICOLON     = r';'
+t_COMMA         = r','
+
+# comparison
+t_LT            = r'<'
+t_GT            = r'>'
+t_LTE           = r'<='
+t_GTE           = r'>='
+t_EQUALVT       = r'==='
+t_NOTEQUALVT    = r'!==' 
+t_EQUALV        = r'==' 
+t_NOTEQUALV     = r'!='
+
+# arithmetic  
+t_PLUSPLUS      = r'\+\+'
+t_MINUSMINUS    = r'--'
+t_PLUS          = r'\+'
+t_MINUS         = r'-'
+t_MULTIPLY      = r'\*'
+t_MODULO        = r'%'
+
+# bitwise
+t_LSHIFT        = r'<<'
+t_RSHIFT        = r'>>'
+t_LOGRSHIFT     = r'>>>'
+t_AND           = r'&'
+t_OR            = r'\|'
+t_XOR           = r'\^'
+t_BWNOT         = r'~'
+
+# logical
+t_NOT           = r'!'
+t_ANDAND        = r'&&'
+t_OROR          = r'\|\|'
+t_TERNARY       = r'\?'
+t_COLON         = r':'
+
+# assignment
+t_EQUAL         = r'='
+t_PLUSEQUAL     = r'\+='
+t_MINUSEQUAL    = r'-='
+t_MULTIPLYEQUAL = r'\*='
+t_MODULOEQUAL   = r'%='
+t_LSHIFTEQUAL   = r'<<='
+t_RSHIFTEQUAL   = r'>>='
+t_LOGRSHIFTEQUAL = r'>>>='
+t_ANDEQUAL      = r'&='
+t_OREQUAL       = r'\|='
+t_XOREQUAL      = r'\^='
+
+# div punctuators
+t_FSLASH        = r'/'
+t_FSLASHEQUAL   = r'/='
+
+t_ignore    = ' \t'
 
 def t_newline(t):
     r'\n+'
--- lexer_tests.py
+++ lexer_tests.py
@@ -2,12 +2,15 @@ import unittest
 from lexer import lexer
 
 class LexerTests(unittest.TestCase):
-    
-    def _validate_identifier_char(self, input):
-        lexer.input(input)
+
+    def _validate_token(self, value, typeName):
+        lexer.input(value)
         token = lexer.token()
-        self.assertEqual(token.value, input)
-        self.assertEqual(token.type, 'IDENTIFIER')
+        self.assertEqual(token.value, value)
+        self.assertEqual(token.type, typeName)
+    
+    def _validate_identifier_char(self, value):
+        self._validate_token(value, 'IDENTIFIER')
 
     def _invalid_first_identifier_char(self, input):
         lexer.input(input)
@@ -15,10 +18,7 @@ class LexerTests(unittest.TestCase):
         self.assertEqual(token, None)
 
     def _validate_keyword(self, keyword):
-        lexer.input(keyword)
-        token = lexer.token()
-        self.assertEqual(token.value, keyword)
-        self.assertEqual(token.type, keyword.upper())
+        self._validate_token(keyword, keyword.upper())
 
     # Comments
     def test_single_line_comment_is_ignored(self):
@@ -110,6 +110,67 @@ class LexerTests(unittest.TestCase):
         for k in future_strict_keywords:
             self._validate_keyword(k)
 
+    def test_punctuators_are_tokenised(self):
+        self._validate_token('{', 'LBRACE')
+        self._validate_token('}', 'RBRACE')
+        self._validate_token('(', 'LPAREN')
+        self._validate_token(')', 'RPAREN')
+        self._validate_token('[', 'LBRACKET')
+        self._validate_token(']', 'RBRACKET')
+        self._validate_token('.', 'DOT')
+        self._validate_token(';', 'SEMICOLON')
+        self._validate_token(',', 'COMMA')
+
+    def test_comparison_punctuators_are_tokenised(self):
+        self._validate_token('<', 'LT')
+        self._validate_token('>', 'GT')
+        self._validate_token('<=', 'LTE')
+        self._validate_token('>=', 'GTE')
+        self._validate_token('==', 'EQUALV')
+        self._validate_token('!=', 'NOTEQUALV')
+        self._validate_token('===', 'EQUALVT')
+        self._validate_token('!==', 'NOTEQUALVT')
+    
+    def test_arithmetic_punctuators_are_tokenised(self):
+        self._validate_token('+', 'PLUS')
+        self._validate_token('-', 'MINUS')
+        self._validate_token('*', 'MULTIPLY')
+        self._validate_token('%', 'MODULO')
+        self._validate_token('++', 'PLUSPLUS')
+        self._validate_token('--', 'MINUSMINUS')
+
+    def test_bitwise_punctuators_are_tokenised(self):
+        self._validate_token('<<', 'LSHIFT')
+        self._validate_token('>>', 'RSHIFT')
+        self._validate_token('>>>', 'LOGRSHIFT')
+        self._validate_token('&', 'AND')
+        self._validate_token('|', 'OR')
+        self._validate_token('^', 'XOR')
+        self._validate_token('~', 'BWNOT')
+
+    def test_logical_punctuators_are_tokenised(self):
+        self._validate_token('!', 'NOT')
+        self._validate_token('&&', 'ANDAND')
+        self._validate_token('||', 'OROR')
+        self._validate_token('?', 'TERNARY')
+        self._validate_token(':', 'COLON')
+
+    def test_assignment_punctuators_are_tokenised(self):
+        self._validate_token('=', 'EQUAL')
+        self._validate_token('+=', 'PLUSEQUAL')
+        self._validate_token('-=', 'MINUSEQUAL')
+        self._validate_token('*=', 'MULTIPLYEQUAL')
+        self._validate_token('%=', 'MODULOEQUAL')
+        self._validate_token('<<=', 'LSHIFTEQUAL')
+        self._validate_token('>>=', 'RSHIFTEQUAL')
+        self._validate_token('>>>=', 'LOGRSHIFTEQUAL')
+        self._validate_token('&=', 'ANDEQUAL')
+        self._validate_token('|=', 'OREQUAL')
+        self._validate_token('^=', 'XOREQUAL')
+
+    def test_div_punctuators_are_tokenised(self):
+        self._validate_token('/', 'FSLASH')
+        self._validate_token('/=', 'FSLASHEQUAL')
 
 if __name__ == '__main__':
     unittest.main()
